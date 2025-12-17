6d41d2052c09a58a2feb1b4b46c754b9d0591cea
Saulius Petraitis <saulius.petraitis@vssa.lt>
2025-12-17 16:08:34 +0200

    Initial commit: documentation structure setup

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+﻿import os
+import sys
+from datetime import date
+
+project = 'UDTS Sąveikos Vadovas'
+copyright = f'{date.today().year}, Valstybės duomenų agentūra'
+author = 'Valstybės duomenų agentūra'
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    'myst_parser',  # Leidžia naudoti Markdown
+    'sphinx_rtd_theme',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+language = 'lt'
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- MyST Parser configuration -----------------------------------------------
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+]
+myst_heading_anchors = 3
---
+++ docs/source/index.rst
@@ -0,0 +1,21 @@
+.. UDTS Sąveikos Vadovas documentation master file.
+
+UDTS Sąveikos Vadovas
+=====================
+
+Sveiki atvykę į UDTS (Universalios duomenų teikimo sąsajos) techninę dokumentaciją.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Turinys:
+
+   ivadas
+   architektura
+   dsa_kurimas
+   diegimas
+
+Indeksai ir paieška
+===================
+
+* :ref:`genindex`
+* :ref:`search`
